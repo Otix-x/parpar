8101a67dbb56075087e064fcdc8a75e948b629e6
igdev116 <minhhunghuynh1106@gmail.com>
2022-07-23 20:26:36 +0700

    fix: UI

--- src/layouts/components/Header.jsx
+++ src/layouts/components/Header.jsx
@@ -12,9 +12,7 @@ const Header = () => {
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-[#F7ECDE]'>
       <Container className='flex flex-wrap gap-4 items-center py-2'>
-        <Link
-          to='/'
-          className='flex items-center mr-6 cursor-pointer select-none'>
+        <Link to='/' className='flex items-center mr-6 cursor-pointer select-none'>
           <img src={logo} className='pt-2 w-14 md:w-auto' />
           <span className='text-xl md:text-4xl text-primary'>parpar___</span>
         </Link>
@@ -22,37 +20,27 @@ const Header = () => {
           <Link to='/' className='transition cursor-pointer hover:text-primary'>
             HOME
           </Link>
-          <Link
-            to='/puppies'
-            className='transition cursor-pointer hover:text-primary'>
+          <Link to='/puppies' className='transition cursor-pointer hover:text-primary'>
             PUPPIES
           </Link>
-          <Link
-            to='/kitties'
-            className='transition cursor-pointer hover:text-primary'>
+          <Link to='/kitties' className='transition cursor-pointer hover:text-primary'>
             KITTIES
           </Link>
-          <Link
-            to='/products'
-            className='transition cursor-pointer hover:text-primary'>
+          <Link to='/products' className='transition cursor-pointer hover:text-primary'>
             PRODUCTS
           </Link>
-          <li className='transition cursor-pointer hover:text-primary'>
-            ABOUT US
-          </li>
-          <li className='transition cursor-pointer hover:text-primary'>
-            CONTACT
-          </li>
+          <li className='transition cursor-pointer hover:text-primary'>ABOUT US</li>
+          <li className='transition cursor-pointer hover:text-primary'>CONTACT</li>
         </ul>
         <div className='flex gap-6 ml-auto'>
-          <Link to='/cart'>
+          <Link to='/cart' className='relative'>
             <img className='w-6 h-6 hover:fill-white' src={shopping} />
+            {getCartItemsCount() !== 0 && (
+              <div className='absolute -top-2 left-[calc(100%-10px)] font-bold text-xs rounded-full text-white px-2 py-0.5 bg-red-400 flex items-center justify-center font-mono'>
+                {getCartItemsCount()}
+              </div>
+            )}
           </Link>
-          {getCartItemsCount() !== 0 && (
-            <div className='font-bold rounded-full text-white px-3 py-0.5 bg-red-400 flex items-center justify-center font-mono'>
-              {getCartItemsCount()}
-            </div>
-          )}
         </div>
       </Container>
     </header>
